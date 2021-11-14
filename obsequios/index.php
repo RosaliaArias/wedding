78463067b72398b51e7e05d9aaf199955d295f53
oliver9321 <oliver9321@gmail.com>
2021-11-13 23:50:56 -0400

    Mostrando articulos en los obsequios

---
+++ obsequios/index.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Dewing&Rosalia</title>
+    <style type="text/css">
+        img.wp-smiley,
+        img.emoji {
+            display: inline !important;
+            border: none !important;
+            box-shadow: none !important;
+            height: 1em !important;
+            width: 1em !important;
+            margin: 0 .07em !important;
+            vertical-align: -0.1em !important;
+            background: none !important;
+            padding: 0 !important;
+        }
+    </style>
+
+    <link rel='stylesheet' id='glanztheme-library-css' href='../wp-content/themes/glanztheme/css/glanztheme_library.css' type='text/css' media='all' />
+    <link rel='stylesheet' id='themify-icons-css' href='../wp-content/themes/glanztheme/fonts/themify-icons.css' type='text/css' media='all' />
+    <link rel='stylesheet' id='marsha-font-css' href='../wp-content/themes/glanztheme/fonts/marsha/stylesheet.css' type='text/css' media='all' />
+    <link rel='stylesheet' id='glanztheme-main-styles-css' href='../wp-content/themes/glanztheme/css/glanztheme_style.css' type='text/css' media='all' />
+    <link rel='stylesheet' id='glanztheme-style-css' href='../wp-content/themes/glanztheme/style.css' type='text/css' media='all' />
+    <link rel='stylesheet' id='fw-ext-breadcrumbs-add-css-css' href='../wp-content/plugins/unyson/framework/extensions/breadcrumbs/static/css/style.css' type='text/css' media='all' />
+    <link rel='stylesheet' id='fw-ext-builder-frontend-grid-css' href='../wp-content/plugins/unyson/framework/extensions/builder/static/css/frontend-grid.css' type='text/css' media='all' />
+    <link rel='stylesheet' id='fw-ext-forms-default-styles-css' href='../wp-content/plugins/unyson/framework/extensions/forms/static/css/frontend.css' type='text/css' media='all' />
+    <link rel='stylesheet' id='fw-shortcode-section-background-video-css' href='../wp-content/plugins/unyson/framework/extensions/shortcodes/shortcodes/section/static/css/background.css' type='text/css' media='all' />
+    <link rel='stylesheet' id='fw-shortcode-section-css' href='../wp-content/plugins/unyson/framework/extensions/shortcodes/shortcodes/section/static/css/styles.css' type='text/css' media='all' />
+    <link rel='stylesheet' id='font-awesome-css' href='../wp-content/plugins/unyson/framework/static/libs/font-awesome/css/font-awesome.min.css' type='text/css' media='all' />
+   <style>
+        img.crazy_lazy {
+            opacity: 0
+        }
+    </style>
+    <style></style>
+</head>   
+
+<body class="page-template page-template-template-one-page page-template-template-one-page-php page page-id-288">
+    <div class="gla_page gla_middle_titles" id="gla_page">
+        <a href="#gla_page" class="gla_top ti ti-angle-up gla_go"></a>
+        <header class="gla_header">
+            <nav class="gla_light_nav gla_transp_nav ">
+                <div class="container">
+                    <div class="gla_logo_container clearfix">
+                        <div class="gla_logo_txt"> <a href="#" class="gla_logo">DEWIN &amp; ROSALIA</a>
+                            <div class="gla_logo_und">Diciembre 18, 2021</div>
+                        </div>
+                    </div>
+                    <div class="gla_main_menu gla_main_menu_mobile">
+                        <div class="gla_main_menu_icon"> <i></i><i></i><i></i><i></i> <b>Menu</b> <b class="gla_main_menu_icon_b">Back</b> </div>
+                    </div>
+                    <div class="gla_main_menu_content gla_image_bck" data-color="rgba(0,0,0,0.9)" data-image="">
+                        <div class="gla_over" data-color="#000" data-opacity="0.7"></div>
+                    </div>
+                    <div class="gla_main_menu_content_menu gla_wht_txt text-right">
+                        <div class="container">
+                            <div class="menu-menu-1-container">
+                                <ul id="menu-menu-1" class="menu">
+                                    <li id="menu-item-766" class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-137 current_page_item menu-item-766"><a href="../index.html">Inicio</a></li>
+                                    <li id="menu-item-779" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-779"><a href="../friends-blocks/index.html">Regalos</a></li>
+                                </ul>
+                            </div>
+                            <div class="gla_main_menu_content_menu_copy"> </div>
+                        </div>
+                    </div>
+                    <div class="gla_search_block"> </div>
+                    <div class="gla_default_menu">
+                        <div class="menu-menu-1-container">
+                            <ul id="menu-menu-2" class="menu">
+                                <li class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-137 current_page_item menu-item-766"><a href="../index.html">Inicio</a></li>
+                                <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-779"><a href="../friends-blocks/index.html">Regalos</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        <section class="gla_slider gla_image_bck gla_fixed gla_wht_txt" data-stellar-background-ratio="0.2" data-image="https://www.adn-mundo.com/wp-content/uploads/2021/08/regalos-de-boda.jpg" data-color="">
+            <div class="gla_over" data-gradient="" data-color="#282828" data-opacity="0.5"></div>
+            <div class="container">
+                <div class="gla_slide_txt gla_slide_center_middle text-center">
+                    <p><img class="gla_animated_flower" src="../wp-content/themes/glanztheme/images/animations/flowers.gif" data-top-bottom="@src:http://glanz.starkethemes.com/wp-content/themes/glanztheme/images/animations/flowers.gif" height="180" style="height: 180px!important"
+                            alt="Glanz"></p>
+                    <div class="gla_slide_title">Espacio de Obsequios</div>
+                </div>
+            </div>
+            <a class="gla_scroll_down gla_go" href="#gla_content"> <b></b> Scroll </a>
+        </section>
+        <section id="gla_content" class="gla_content">
+            <div class="fw-page-builder-content">
+                <section class=" gla_section gla_image_bck " style=background-color:#ffffff;>
+                    <div class="container text-center">
+                        <h2 class="gla_h2_title">Obsequiar en 3 pasos</h2>
+                        <div class="row gla_icon_boxes">
+                            <div class="gla_icon_box fw-col-sm-4 text-center col-xs-12"> <i class="ti flaticon-016-calendar"></i>
+                                <div class="gla_icon_box_content">
+                                    <h4> <b> PASO 1</b> </h4>Elegir el obsequio con el que más te identifiques y reservarlo</div>
+                            </div>
+                            <div class="gla_icon_box fw-col-sm-4 text-center col-xs-12"> <i class="ti flaticon-041-cake"></i>
+                                <div class="gla_icon_box_content">
+                                    <h4> <b> PASO 2 </b> </h4>Dirigirte a la tienda, preguntar por el artículo y comprarlo</div>
+                            </div>
+
+                            <div class="gla_icon_box fw-col-sm-4 text-center col-xs-12"> <i class="ti flaticon-006-hand"></i>
+                                <div class="gla_icon_box_content">
+                                    <h4> <b> PASO 3 </b> </h4>Llevar el obsequio a la ceremonia</div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section class=" gla_section gla_image_bck " style=background-color:#f2f2f2;>
+                    <div class="container text-center">
+                        <h2 class="gla_h2_title">Obsequios de Boda</h2>
+                        <div class="button-group filter-button-group">
+
+                        <?php
+                                $obj = json_decode(file_get_contents('http://dyrwedding.com/sistema/index.php?c=categorias&a=getJsonCategorias'), true);
+                                echo '<a data-filter="*">Mostrar todos</a>';
+                                foreach ($obj as $key => $value) {
+                                    if($value['Categoria']){
+                                       echo '<a data-filter=".'.$value['Id'].'">'.$value['Categoria'].'</a> ';
+                                    }
+                                }
+                            ?>
+
+                        </div>
+                        <div class="gla_portfolio grid">
+                        <?php $obj2 = json_decode(file_get_contents('http://dyrwedding.com/sistema/index.php?c=regalos&a=getJsonRegalos'), true); ?>
+                               
+                                <?php foreach ($obj2  as $key => $value) : ?>
+
+                                    <div class="col-sm-4 gla_anim_box grid-item <?= $value['Id'] ?>">
+                                        <div class="gla_shop_item">
+                                            <a href="../wp-content/uploads/2017/06/14214839668_c094232d97_k-1200x800.jpg" class="lightbox"> 
+                                                <img src="http://dyrwedding.com/sistema/<?= $value['Imagen']?>" alt="Glanz" width="80"> </a>
+                                                 <span class="gla_shop_item_title"><?= $value['Titulo']?></span> 
+                                         </div>
+                                    </div>
+                                <?php endforeach; ?>
+                            
+
+                           <!-- <div class="col-sm-4 gla_anim_box grid-item Bridesmaids">
+                                <div class="gla_shop_item">
+                                    <a href="../wp-content/uploads/2017/06/14421186043_a817e20aa5_k-1200x800.jpg" class="lightbox"> <img src="../wp-content/uploads/2017/06/14421186043_a817e20aa5_k-700x467.jpg" alt="Glanz"> </a> <span class="gla_shop_item_title"> Andrew &amp; Joanna </span> </div>
+                            </div>
+                           
+                            <div class="col-sm-4 gla_anim_box grid-item ">
+                                <div class="gla_shop_item">
+                                    <a href="../wp-content/uploads/2017/06/14421584103_4393f08b6c_k-1200x800.jpg" class="lightbox"> <img src="../wp-content/uploads/2017/06/14421584103_4393f08b6c_k-700x467.jpg" alt="Glanz"> </a> <span class="gla_shop_item_title"> Izzy &amp; Katy </span> </div>
+                            </div>-->
+
+                        </div>
+                    </div>
+                </section>
+
+                <section class=" gla_section gla_image_bck gla_section_sml_padding" style=background-color:#e5e5e5;>
+                    <div class="container ">
+                        <div class="fw-col-xs-12 fw-col-sm-4 gla_bordered_block gla_grey_border gla_col">
+                            <div class=" ">
+                                <div class="gla_text_block">
+                                    <p>2021 © Devsarrollando.org </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="fw-col-xs-12 fw-col-sm-4 enh_push_none gla_bordered_block gla_grey_border text-center gla_col">
+                            <div class=" ">
+                                <div class="gla_social_btns">
+                                     <a href="#"><i class="ti ti-facebook gla_icon_box"></i></a> 
+                                     <a href="#"><i class="ti ti-instagram gla_icon_box"></i></a>
+                            </div>
+                        </div>
+                        <div class="fw-col-xs-12 fw-col-sm-4 gla_bordered_block gla_grey_border gla_col">
+                            <div class=" "> </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </section>
+    </div>
+    <script type='text/javascript' src='.././ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js' defer='defer'></script>
+    <script type='text/javascript'>
+        try {
+            jQuery.noConflict();
+        } catch (e) {};
+    </script>
+    <script type='text/javascript' src='../wp-includes/js/jquery/jquery-migrate.min.js' defer='defer'></script>
+    <!--<script type='text/javascript' src='.././ajax.aspnetcdn.com/ajax/jquery.validate/1.15.0/jquery.validate.min.js' defer='defer'></script>-->
+    <script type='text/javascript' src='../wp-content/plugins/rsvp/rsvp_plugin.js' defer='defer'></script>
+    <script type='text/javascript' src='../wp-content/plugins/contact-form-7/includes/js/scripts.js' defer='defer'></script>
+    <script type='text/javascript' src='../wp-includes/js/imagesloaded.min.js' defer='defer'></script>
+    <script type='text/javascript' src='../wp-content/themes/glanztheme/js/glanztheme_library.js' defer='defer'></script>
+    <script type='text/javascript' src='../wp-content/plugins/unyson/framework/static/js/fw-form-helpers.js' defer='defer'></script>
+    <script type='text/javascript' src='../wp-content/themes/glanztheme/js/navigation.js' defer='defer'></script>
+    <script type='text/javascript' src='../wp-content/themes/glanztheme/js/skip-link-focus-fix.js' defer='defer'></script>
+    <script type='text/javascript' src='.././ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js' defer='defer'></script>
+    <script type='text/javascript' src='../wp-content/themes/glanztheme/js/glanztheme_script.js' defer='defer'></script>
+    <script type='text/javascript' src='../wp-content/plugins/speed-booster-pack/inc/js/jquery.unveil.min.js' defer='defer'></script>
+    <script type='text/javascript' src='../wp-includes/js/wp-embed.min.js' defer='defer'></script>
+
+    <script>
+
+    </script>
+</body>
+</html>
